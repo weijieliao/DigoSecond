c37472f5d947922ffffa90f66246e93fcd9e778f
chenh <397314391@qq.com>
2016-10-26 23:26:51 +0800

    third commit

--- app/src/main/java/com/example/lwj/digosecond/MainActivity.java
+++ app/src/main/java/com/example/lwj/digosecond/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String name = "chenh";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

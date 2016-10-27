ab7996ee97f23fbef6fec4db4ebf440275060c3c
weijieliao <949623493@qq.com>
2016-10-27 09:31:54 +0800

    second commit

--- app/src/main/java/com/example/lwj/digosecond/MainActivity.java
+++ app/src/main/java/com/example/lwj/digosecond/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String str = "test branche" ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
